8c9a21d7e058edb990f156b1fcb6280d53b2bb82
Thinus <thinus.barnard@payfast.co.za>
2020-06-26 16:15:19 +0200

    add bootstrap and tidy up fe

--- application.py
+++ application.py
@@ -1,21 +1,27 @@
-from flask import Flask, render_template, request, jsonify
-# import numpy as np
+from flask import Flask, render_template, request, jsonify,redirect,url_for
 import requests
 
 app = Flask(__name__)
-app_url = 'http://63ec2666f911.ngrok.io'
+app_url = 'https://c8cdd0ab0585.ngrok.io'
+app_data_file = 'payfast_data.txt'
 
 @app.route("/")
 def index():
-    return render_template('index.html', app_url=app_url)
+    lines = open(app_data_file, 'r').read().split('\n')
+    return render_template('index.html', app_url=app_url, purchase=lines)
+
+@app.route("/clear")
+def clear():
+    f = open(app_data_file, "w")
+    f.close()
+    return redirect(url_for('index') , code=302)
 
 @app.route("/receive_payment", methods=["GET", "POST"])
 def receive_payment():
     data = request.get_data().decode().split('&')
     for i in range(0,len(data)):
         data[i] = data[i].split('=')
-    f = open("payfast_data.txt", "a")
-    # f.write(data)
+    f = open(app_data_file, "w")
 
     f.write("======================================================================================================\n")
     for key, val in data:
@@ -23,4 +29,4 @@ def receive_payment():
 
     f.write("======================================================================================================\n")
     f.close()
-    return render_template('forward.html', product=data)
+    return jsonify({'code': '200', 'result': "Success"})
